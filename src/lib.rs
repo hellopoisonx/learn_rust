b533a8a992a133aa4cc4530ff7bdfb17aaef4dbb
hellopoisonx <x1665341912@gmail.com>
2023-12-31 16:08:27 +0800

    web_server

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,31 @@
-use std::fmt::Display;
-use std::ptr::null_mut;
-struct Node<T: Copy + Display> {
-    val: T,
-    next: *mut Node<T>,
+use std::thread;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
 }
 
-pub struct LinkedList<T: Copy + Display> {
-    head: *mut Node<T>,
-}
-impl<T: Copy + Display> std::default::Default for LinkedList<T> {
-    fn default() -> Self {
-        Self { head: null_mut() }
+impl Worker {
+    fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+        Self { id, thread }
     }
 }
-impl<T: Copy + Display> LinkedList<T> {
-    pub fn new(val: T) -> Self {
-        let new_node = Node {
-            val,
-            next: null_mut(),
-        };
-        Self {
-            head: Box::into_raw(Box::new(new_node)),
+impl ThreadPool {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        for i in 0..size {
+            workers.push(Worker::new(i))
         }
+        Self { workers }
     }
-    pub fn push(&mut self, val: T) {
-        let new_node = Node {
-            val,
-            next: self.head,
-        };
-        self.head = Box::into_raw(Box::new(new_node));
-    }
-    pub fn pop(&mut self) -> Option<T> {
-        unsafe {
-            if self.head.is_null() {
-                return None;
-            }
-            let tmp = (*self.head).val;
-            let tmp_head = (*self.head).next;
-            let _ = Box::from_raw(self.head);
-            self.head = tmp_head;
-            Some(tmp)
-        }
-    }
-    pub fn pop_back(&mut self) -> Option<T> {
-        let mut current = self.head;
-        let mut prev = null_mut();
-        unsafe {
-            while !(*current).next.is_null() {
-                prev = current;
-                current = (*current).next;
-            }
-            if !prev.is_null() {
-                (*prev).next = null_mut();
-            } else if !self.head.is_null() {
-                self.head = null_mut();
-            } else {
-                return None;
-            }
-            let tmp = (*current).val;
-            let _ = Box::from_raw(current);
-            Some(tmp)
-        }
-    }
-    pub fn display(&self) {
-        let mut current = self.head;
-        if current.is_null() {
-            return;
-        }
-        unsafe {
-            while !(*current).next.is_null() {
-                print!("{}=>", (*current).val);
-                current = (*current).next;
-            }
-            if !current.is_null() {
-                print!("{}\n", (*current).val);
-            }
-        }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        f();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-use learn_rust::LinkedList;
+use learn_rust::ThreadPool;
+use std::{
+    fs,
+    io::Write,
+    net::{TcpListener, TcpStream},
+    thread,
+};
+
 fn main() {
-    let mut l = LinkedList::new("H");
-    l.push("hello");
-    l.push("hello");
-    l.push("hello");
-    l.display();
-    l.pop();
-    l.display();
-    l.pop();
-    l.display();
-    l.pop();
-    l.display();
+    let listener = TcpListener::bind("localhost:7777").unwrap();
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(move || {
+            handle_connection(stream);
+        })
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let content = fs::read_to_string("hello.html").unwrap();
+    let resp = format!("HTTP/1.1 200 OK\r\n\r\n{}", content);
+    stream.write_all(resp.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
